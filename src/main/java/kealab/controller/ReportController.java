96c13b4ab2b40416ca05108e80b2ac0f9cc842e8
XtheWiz <xthewiz.dev@gmail.com>
2017-05-19 15:29:07 +0700

    Add CORS support

--- src/main/java/kealab/controller/ReportController.java
+++ src/main/java/kealab/controller/ReportController.java
@@ -2,6 +2,7 @@ package kealab.controller;
 
 import kealab.domain.DummyModel;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,22 +15,23 @@ import java.util.Properties;
 /**
  * Created by xthewiz on 5/16/2017 AD.
  */
+@CrossOrigin(origins = "*")
 @RestController
 public class ReportController {
 
-    Properties prop = new Properties();
-    InputStream inputStream = null;
-    String reportJar;
-
     @RequestMapping(value = "/report")
     public DummyModel makeReport(
-            @RequestParam(value = "appId", required = false, defaultValue = "none") String appId) throws IOException {
+            @RequestParam(value = "appId", required = false, defaultValue = "none") String appId) throws IOException, InterruptedException {
+        Properties prop = new Properties();
+        InputStream inputStream = null;
+        String reportJar;
+
         inputStream = new FileInputStream("config.properties");
         prop.load(inputStream);
         reportJar = prop.getProperty("report_jar");
 
         System.out.println("Target jar file : " + reportJar);
-        System.out.println("App ID : " + appId);
+        System.out.println("App Name : " + appId);
         DummyModel dummyModel = new DummyModel();
         dummyModel.setId(1);
 
@@ -48,12 +50,21 @@ public class ReportController {
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String s = "";
         while ((s = reader.readLine()) != null) {
+            System.out.println(s);
             respList.add(s);
+
+            if(s.contains("FINISH Report Batch Application")) {
+                break;
+            }
         }
         dummyModel.setMessages(respList);
 
-        System.out.println(dummyModel.getMessages());
-        return dummyModel;
+        int status = process.waitFor();
 
+        dummyModel.getMessages().add("--------- Finish ["+ status +"]--------------------------");
+        System.out.println("Exit status : " + status);
+
+        process.destroy();
+        return dummyModel;
     }
 }
--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,20 +1,28 @@
-angular.module('kea-lab', [])
+var myApp = angular.module('kea-lab', [])
     .controller('ReportController', function($scope, $http) {
         var process_statuses = {
             'idle' : 'Please click button to generate report',
+            'error' : 'Error Occerrued:',
             'processing' : 'Processing, please wait',
             'finish' : 'Reponse : '
         }
+        var hostname = location.host;
         var report = this;
         $scope.status = process_statuses.idle;
 
         report.makeReport = function() {
+            console.log("Click : " + hostname);
+            $scope.messages = [];
             $scope.status = process_statuses.processing;
-            $http.get("http://localhost:9000/report", {
+            $http.get("http://"+hostname+"/report", {
                 params: {appId: report.appId}
             }).then(function(response){
                 $scope.status = process_statuses.finish;
                 $scope.messages = response.data.messages;
+                console.log(response);
+            }, function(response) {
+                $scope.status = process_statuses.error;
+                $scope.messages = response;
                 console.log(response);
             });
         }
